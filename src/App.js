60fdd65fc9cb089afdb9742b0914e5f40ec5e00a
galisaishankar08 <2010030054@klh.edu.in>
2023-01-23 22:36:03 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Home from './components/Home/Home'
+// import Herdsman from './components/Herdsman/App'
+// import Slaughter from './components/Slaughter/App'
+// import Retailer from './components/Retailer/App'
+// import Consumer from './components/Consumer/App'
+// import Admin from './components/Admin/App'
+import Login from './components/Login/Home'
+import Register from './components/Register/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="signin" element={<Login />} />
+        <Route path="signup" element={<Register />} />
+        {/* <Route index element={<Home/>} />
+        <Route path='admin' element={<Admin/>} />
+        <Route path='herdsman/*' element={<Herdsman/>} />
+        <Route path='slaughter/*' element={<Slaughter/>} />
+        <Route path='retailer/*' element={<Retailer/>} />
+        <Route path='consumer/*' element={<Consumer/>} /> */}
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Footer/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      
+      <div className='bg-red w-full p-5 mt-[97px]'>
+        <div>
+          <ul className='flex pb-3 text-[20px] justify-center text-white font-poppins space-x-12'>
+            <li className='hover:text-red-500'><a href="/about">About Us</a></li>
+            <li className='hover:text-red-500'><a href="/about">Policies</a></li>
+            <li className='hover:text-red-500'><a href="/about">Terms&Conditions</a></li>
+            <li className='hover:text-red-500'><a href="/about">Enquries</a></li>
+          </ul>
+        </div>
+
+        <div className='flex justify-center text-[20px] font-poppins text-white'>
+          <p>© 2022-2023, MLedger.com, Inc. or its affiliates</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer
